344a0b874fa079f260c558db0b93c204bbecc596
Todd Branchflower <todd.branchflower@gmail.com>
2013-10-16 11:16:21 -0600

    my first commit

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ * Author: Todd Branchflower
+ * Date: 11 Oct 2013
+ * Description: Demonstrates functionality of the pong.h library
+ */
+
+#include <stdio.h>
+#include "pong.h"
+
+int main(int argc, const char *argv[])
+{
+    ball_t ball = createBall(400, 420, 30, 40, 10); 
+
+    while (1)
+    {
+        printf("Ball x:%d, y:%d\n", ball.position.x, ball.position.y);
+        sleep(1);
+        ball = moveBall(ball);
+    }
+    return 0;
+}
